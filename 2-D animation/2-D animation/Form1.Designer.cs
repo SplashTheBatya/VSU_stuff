bab67168f474c197c68361ccdc329705b2b666f7
Kakolay Nimalov <47940537+SplashTheBatya@users.noreply.github.com>
2019-12-24 18:20:59 +0300

    АНИМАЦИЯ ПО КГ

---
+++ 2-D animation/2-D animation/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace _2_D_animation
+{
+    partial class animation
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer_a = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox_sunrise = new System.Windows.Forms.PictureBox();
+            this.pictureBox_earth = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_sunrise)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_earth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox_sunrise
+            // 
+            this.pictureBox_sunrise.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox_sunrise.Name = "pictureBox_sunrise";
+            this.pictureBox_sunrise.Size = new System.Drawing.Size(770, 446);
+            this.pictureBox_sunrise.TabIndex = 0;
+            this.pictureBox_sunrise.TabStop = false;
+            // 
+            // pictureBox_earth
+            // 
+            this.pictureBox_earth.Location = new System.Drawing.Point(12, 457);
+            this.pictureBox_earth.Name = "pictureBox_earth";
+            this.pictureBox_earth.Size = new System.Drawing.Size(770, 122);
+            this.pictureBox_earth.TabIndex = 1;
+            this.pictureBox_earth.TabStop = false;
+            // 
+            // animation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 607);
+            this.Controls.Add(this.pictureBox_earth);
+            this.Controls.Add(this.pictureBox_sunrise);
+            this.Name = "animation";
+            this.Text = "2D_animation";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.animation_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_sunrise)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_earth)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer_a;
+        private System.Windows.Forms.PictureBox pictureBox_sunrise;
+        private System.Windows.Forms.PictureBox pictureBox_earth;
+    }
+}
+
---
+++ 2-D animation/2-D animation/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2_D_animation
+{
+    public partial class animation : Form
+    {
+        float sunx;
+        float suny;
+        double movementx = 0;
+        double movementy = 0;
+        double angle = 0;
+        int r = 22;
+        int x0 = 270;
+        int y0 = 480;
+
+        public animation()
+        {
+            InitializeComponent();
+            pictureBox_earth.Image = new Bitmap(pictureBox_earth.Width, pictureBox_earth.Height);
+            Graphics g = Graphics.FromImage(pictureBox_earth.Image);
+            g.Clear(Color.Green);
+            pictureBox_sunrise.Image = new Bitmap(pictureBox_sunrise.Width, pictureBox_sunrise.Height);
+            Graphics gr = Graphics.FromImage(pictureBox_sunrise.Image);
+            gr.Clear(Color.Blue);
+        }
+        public void sunrise(double movementx, double movementy, double angle)
+        {
+            pictureBox_sunrise.Image = new Bitmap(pictureBox_sunrise.Width, pictureBox_sunrise.Height);
+            Graphics g = Graphics.FromImage(pictureBox_sunrise.Image);
+            g.Clear(Color.Blue);
+            //sun
+            sunx =(float)( x0 + movementx);
+            suny = (float)(y0 + movementy);
+            g.FillEllipse(Brushes.Gold, sunx, suny, 70, 70);
+            //rays up/down/left/right
+
+            for(double teta = 0; teta < 2* Math.PI; teta += Math.PI/100)
+            {
+
+                g.DrawLine(new Pen(Brushes.Gold), sunx + 100 * (float)Math.Sin(teta) + 35, suny + 100 * (float)Math.Cos(teta) + 35, sunx + 35, suny + 35);
+            }
+
+            //g.FillRectangle(Brushes.Gold, sunx + 32, suny - 25, sunraywidth, sunraiheight);
+            //g.FillRectangle(Brushes.Gold, sunx + 32, suny + 75, sunraywidth, sunraiheight);
+            //g.FillRectangle(Brushes.Gold, sunx - 30, suny + 30, sunraiheight, sunraywidth);
+            //g.FillRectangle(Brushes.Gold, sunx + 75, suny + 30, sunraiheight, sunraywidth);
+        }
+
+
+        private void animation_KeyDown(object sender, KeyEventArgs e)
+        {
+            pictureBox_sunrise.Image = new Bitmap(pictureBox_sunrise.Width, pictureBox_sunrise.Height);
+            Graphics g = Graphics.FromImage(pictureBox_sunrise.Image);
+            if (e.KeyCode == Keys.Left)
+            {
+
+                angle -= 0.1;
+                movementx += r * Math.Cos(angle);
+                movementy += r * Math.Sin(angle);
+                sunrise(movementx, movementy, angle);
+            }
+            if (e.KeyCode == Keys.Right)
+            {  
+
+                movementx -= r * Math.Cos(angle);
+                movementy -= r * Math.Sin(angle);
+                angle += 0.1;
+                sunrise(movementx, movementy, angle);
+            }
+            else
+            {
+                
+            }
+                
+           
+        }
+      
+    }
+}
